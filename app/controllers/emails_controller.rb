6dab3835fee74c045b119d4d9c4ff674ed61078c
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-09-18 13:09:53 -0400

    emailCreate

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -3,4 +3,19 @@ class EmailsController < ApplicationController
         emails = Email.all 
         render json: emails
     end 
+
+    def new
+        email = Email.new
+    end 
+
+    def create
+        email = Email.craete(email_params)
+        render json: email.to_json
+    end 
+
+    private 
+
+    def email_params
+        params.require(:email).permit(:sender_id, :reciever_id, :subject_line, :content, :read )
+    end 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.destroy_all 
+# User.destroy_all 
 Email.destroy_all
 InstantMessage.destroy_all
 Interest.destroy_all
@@ -7,11 +7,11 @@ AwayMessage.destroy_all
 
 boolean = [true, false]
 
-10.times do 
-    response = RestClient.get('https://randomuser.me/api/')
-    data = JSON.parse(response)
-    User.create(first_name: data["results"][0]["name"]["first"], last_name: data["results"][0]["name"]["last"], age: data["results"][0]["dob"]["age"], email: data["results"][0]["email"], location: data["results"][0]["location"]["city"], picture: data["results"][0]["picture"]["large"], password: data["results"][0]["login"]["password"], isOnline: boolean.sample, username: Faker::Internet.username)
-    end 
+# 10.times do 
+#     response = RestClient.get('https://randomuser.me/api/')
+#     data = JSON.parse(response)
+#     User.create(first_name: data["results"][0]["name"]["first"], last_name: data["results"][0]["name"]["last"], age: data["results"][0]["dob"]["age"], email: data["results"][0]["email"], location: data["results"][0]["location"]["city"], picture: data["results"][0]["picture"]["large"], password: data["results"][0]["login"]["password"], isOnline: boolean.sample, username: Faker::Internet.username)
+#     end 
 
 900.times do 
     Email.create(read: boolean.sample, sender_id: User.all.sample.id, reciever_id: User.all.sample.id, subject_line: Faker::Beer.hop, content: Faker::Movies::PrincessBride.quote )
